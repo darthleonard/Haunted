4da0b94c6c4a9f2afb4967623b4c59f552cb3cb1
darthleonard <leolinuxmx@gmail.com>
2021-05-10 19:18:56 -0500

    test map load

---
+++ sources/16x16/maps/Overworld1.tsx
@@ -0,0 +1,67 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="Overworld" tilewidth="16" tileheight="16" tilecount="1440" columns="40">
+ <image source="../images/Overworld.png" width="640" height="576"/>
+ <terraintypes>
+  <terrain name="Nuevo Terreno" tile="0"/>
+  <terrain name="Nuevo Terreno" tile="0"/>
+  <terrain name="Nuevo Terreno" tile="0"/>
+ </terraintypes>
+ <tile id="203" terrain=",,,2"/>
+ <tile id="244" terrain="2,,,"/>
+ <tile id="246" terrain=",,2,"/>
+ <tile id="286" terrain="2,2,,"/>
+ <tile id="287" terrain=",,2,"/>
+ <tile id="371" terrain=",,2,2"/>
+ <tile id="372" terrain=",2,2,"/>
+ <tile id="407" terrain="2,2,,"/>
+ <tile id="408" terrain="2,2,,"/>
+ <tile id="409" terrain=",2,,"/>
+ <tile id="410" terrain=",,2,"/>
+ <tile id="451" terrain=",2,,"/>
+ <tile id="452" terrain=",,2,"/>
+ <tile id="453" terrain="2,,2,2"/>
+ <tile id="454" terrain=",,2,2"/>
+ <tile id="455" terrain=",,2,2"/>
+ <tile id="456" terrain=",,,2"/>
+ <tile id="457" terrain=",,2,2"/>
+ <tile id="458" terrain=",,2,"/>
+ <tile id="493" terrain=",2,,"/>
+ <tile id="494" terrain="2,,2,"/>
+ <tile id="495" terrain=",,2,"/>
+ <tile id="497" terrain=",2,,"/>
+ <tile id="498" terrain="2,2,,2"/>
+ <tile id="499" terrain="2,,,"/>
+ <tile id="535" terrain=",2,2,2"/>
+ <tile id="536" terrain=",,2,2"/>
+ <tile id="537" terrain=",,2,"/>
+ <tile id="577" terrain="2,2,,2"/>
+ <tile id="578" terrain=",2,,"/>
+ <tile id="612" terrain=",,2,"/>
+ <tile id="618" terrain=",2,,"/>
+ <tile id="650" terrain=",,2,2"/>
+ <tile id="651" terrain=",,2,"/>
+ <tile id="659" terrain=",2,,"/>
+ <tile id="660" terrain=",,,2"/>
+ <tile id="663" terrain="2,,,"/>
+ <tile id="694" terrain="2,,,"/>
+ <tile id="696" terrain=",2,,"/>
+ <tile id="698" terrain=",2,,"/>
+ <tile id="700" terrain=",,2,"/>
+ <tile id="701" terrain="2,,,2"/>
+ <tile id="702" terrain=",,2,"/>
+ <tile id="704" terrain=",,2,"/>
+ <tile id="742" terrain=",2,,"/>
+ <tile id="743" terrain="2,,2,"/>
+ <tile id="745" terrain=",2,,"/>
+ <tile id="747" terrain="2,,,"/>
+ <tile id="748" terrain=",,2,2"/>
+ <tile id="783" terrain=",2,,"/>
+ <tile id="784" terrain=",,2,"/>
+ <tile id="823" terrain="2,2,,"/>
+ <tile id="824" terrain="2,,,"/>
+ <tile id="861" terrain=",,2,"/>
+ <tile id="978" terrain=",2,,2"/>
+ <tile id="979" terrain="2,2,2,"/>
+ <tile id="980" terrain=",,2,"/>
+ <tile id="981" terrain=",,2,2"/>
+</tileset>
